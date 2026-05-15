9aaa922475e28b0f2871e673309a2b1342347437
Irina <attame@yandex.ru>
2026-05-15 18:40:43 +0500

    try with resources, .read()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,46 +1,24 @@
 package org.example;
 
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        File directory = new File("folder2"); // задаёт имя папки
-        File directoryes = new File("dir/folder/folder1"); // задаёт имена иерархии папок
-        File file = new File("dir/folder/folder1/file1.txt"); // описывает путь к файлу и назначает имя
-        File file2 = new File("dir/folder/folder1/file2.txt");
-        File file5 = new File("folder2/file5.txt");
-        File file4 = new File("folder2/file4.txt");
-
-
-
+        File file = new File("1.txt");
         try {
-            directoryes.mkdirs(); // создаёт иерархию папок
-            directory.mkdir(); // создаёт новую папку
-            file.createNewFile(); // создаёт новый файл
-            file2.createNewFile();
-            file5.createNewFile();
-            file4.createNewFile();
+            file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        File[] fold2 = directory.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.contains("4");
+        try(InputStream inputStream = new FileInputStream(file)) { // создаём поток в конструкции try
+            int a = inputStream.read(); // читаем и записываем значение байта
+            while (a != -1) {
+                System.out.print((char) a);
+                a = inputStream.read(); // читаем следующий символ
             }
-        });
-        File[] fold1 = directoryes.listFiles(); // получаем массив файлов лежащих в директории
-
-        for (File f : fold1){ // выводим список файлов из директории
-            System.out.println(f.getName());
+        } catch (Exception e) { // будем ловить все исключения
+            throw new RuntimeException(e);
         }
-
-        for (File f : fold2){ // выводим список файлов из директории
-            System.out.println("только 4 " + f.getName());
-        }
-
     }
 }
