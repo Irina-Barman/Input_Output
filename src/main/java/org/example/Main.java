b9ac83c9dac33b119b2c1dda3eaf757f024893f8
Irina <attame@yandex.ru>
2026-05-16 19:21:04 +0500

    читаем массив с помощью .append(new String(array, 0,count));

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,17 +14,14 @@ public class Main {
             e.printStackTrace();
         }
         try (InputStream inputStream = new FileInputStream(file)) {
-            long before = System.currentTimeMillis();
-            byte[] array = new byte[1024]; // Создадим массив на 1 тыс. байт
+            byte[] array = new byte[8]; // Создадим массив на 1 тыс. байт
             int count = inputStream.read(array); // передаём массив, получает кол-во элементов которое было прочитано
             StringBuilder result = new StringBuilder();
             while (count > 0) { // читаем до тех пор, пока кол-во элементов > 0
-                result.append(new String(array)); // создаем строку из массива байт
+                result.append(new String(array, 0,count)); // создаем строку из массива байт
                 count = inputStream.read(array);
             }
             System.out.println(result.toString()); // преобразуем в строку
-            long after = System.currentTimeMillis();
-            System.out.println("Time - " + (after - before));
 
         } catch (Exception e) {
             e.printStackTrace();
