04f3b6047dd6164d0b917ce63947bce17ae22291
Irina <attame@yandex.ru>
2026-05-23 19:41:16 +0500

    solving the task of lesson 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,27 +8,19 @@ public class Main {
     public static void main(String[] args) {
         File directory = new File("folder");
         File file = new File(directory, "names.txt");
-        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file, true))) {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter name or \"stop\" to exit");
-            String next = scanner.nextLine();
-            while (!next.equals("stop")) {
-                outputStream.write(next.getBytes());
-                outputStream.write("\n".getBytes());
-                next = scanner.nextLine();
+            System.out.println("Enter next page or \"stop\" to exit");
+            byte[] pageText = new byte[3000];
+            String input = scanner.nextLine();
+            while (!input.equals("stop")) {
+                int page = Integer.parseInt(input);
+                randomAccessFile.seek((page - 1) * pageText.length);
+                int count = randomAccessFile.read(pageText);
+                System.out.println(new String(pageText, 0, count));
+                System.out.println("Enter next page or \"stop\" to exit");
+                input = scanner.nextLine();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
-            byte[] array = new byte[128];
-            int count = inputStream.read(array);
-            StringBuilder result = new StringBuilder();
-            while (count > 0) {
-                result.append(new String(array, 0, count));
-                count = inputStream.read(array);
-            }
-            System.out.println(result.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
